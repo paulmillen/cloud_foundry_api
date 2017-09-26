0cbb73c240bbe8e3bd390966700654682b30b203
paulmillen <pwmillen@gmail.com>
2017-09-26 14:34:51 +0100

    test parsing string to json

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,8 +4,9 @@ describe 'The API App' do
 
     it 'returns a string containing json' do
       get '/v2/catalog'
+      return_hash = JSON.parse(last_response.body)
       expect(last_response).to be_ok
-      expect(last_response.body).to eq('{"services": []}')
+      expect(return_hash).to eq( {"services" => []} )
     end
 
 end
